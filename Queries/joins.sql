016d0d92a5d635940d7724b4fd9a1b063393eafa
Jforbus <109490597+Jforbus@users.noreply.github.com>
2022-09-18 16:54:57 -0500

    Joined and Counted Retirement Eligibility by Dept.

---
+++ Queries/joins.sql
@@ -0,0 +1,21 @@
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- JOINING RETIREMENT INFO AND DEPT EMP TABLES
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp
