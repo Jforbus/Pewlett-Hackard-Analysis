e8f806dc2cc77d2d51df7a00962e585ac4b8e045
Jforbus <109490597+Jforbus@users.noreply.github.com>
2022-09-18 15:53:37 -0500

    Created Database, Queried for Retirement Eligiblity

--- Data/EmployeeDB_Schema.sql
+++ Data/EmployeeDB_Schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE dept_manager (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE salaries (
@@ -36,13 +36,25 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+DROP TABLE titles CASCADE;
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-SELECT * FROM departments;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+SELECT * FROM dept_manager;
